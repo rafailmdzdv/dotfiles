bb1e1c207ad0e071efa954bba0f91771c3cc04ae
rafailmdzdv <rafayt323@xmail.ru>
2024-05-03 11:48:04 +0300

    add markdown preview

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -5,4 +5,13 @@ return {
       require("auto-save").setup()
     end,
   },
+
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
 }
