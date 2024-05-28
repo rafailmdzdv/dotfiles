1db7e8b5dd42b2a264c5ef3f903d5528b080c566
rafailmdzdv <rafayt323@xmail.ru>
2024-05-28 09:38:43 +0300

    fix(nvim): disable flash

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -1,5 +1,9 @@
 return {
   {
+    "folke/flash.nvim",
+    enabled = false,
+  },
+  {
     "nvim-telescope/telescope.nvim",
     keys = {
       { "<leader>fg", "<cmd>Telescope live_grep<CR>", desc = "Live Grep" },
