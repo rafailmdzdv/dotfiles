f7caf3f586625fdf5203a97d7da12d6292293605
rafailmdzdv <rafayt323@xmail.ru>
2024-05-28 15:42:35 +0300

    fix(nvim): set prefix for lsp signature

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,53 +1,8 @@
-require("neodev").setup()
-local lspconfig = require("lspconfig")
-
-local opts = { noremap = true, silent = true }
-local on_attach = function()
-    vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-    vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
-    vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-    vim.keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
-    vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", opts)
-    vim.keymap.set("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-    vim.keymap.set("n", "<leader>D", "<cmd>Telescope lsp_type_definitions<CR>", opts)
-    vim.keymap.set("n", "<leader>rn", require("renamer").rename, opts)
-    vim.keymap.set("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-    vim.keymap.set("n", "<leader>lr", "<cmd>LspRestart<CR>", opts)
-    vim.keymap.set("n", "<leader>d", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-    vim.keymap.set("n", "<leader>dn", "<cmd>lua vim.diagnostic.goto_next({ float = false })<CR>", opts)
-    vim.keymap.set("n", "<leader>dN", "<cmd>lua vim.diagnostic.goto_prev({ float = false })<CR>", opts)
-end
-
-local servers = { "jedi_language_server", "tsserver", "clangd" }
-local lspcmp = require("cmp_nvim_lsp")
-local capabilities = lspcmp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-for _, server in ipairs(servers) do
-  lspconfig[server].setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
-  })
-end
-
--- Lua
-lspconfig.lua_ls.setup({
-  capabilities = capabilities,
-  on_attach = on_attach,
-  settings = {
-      Lua = {
-          diagnostics = { globals = { "vim" } },
-          hint = { enable = true },
-      },
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      opts.diagnostics.virtual_text.prefix = "👺"
+    end,
   },
-})
-
--- Frontend
-lspconfig.html.setup({
-  capabilities = capabilities,
-  on_attach = on_attach,
-})
-
-lspconfig.emmet_ls.setup({
-  capabilities = capabilities,
-  on_attach = on_attach,
-})
+}
