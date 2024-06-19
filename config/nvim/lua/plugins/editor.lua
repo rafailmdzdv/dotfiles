2610b629de5af9188df30f6187fbad09c08000bb
rafailmdzdv <rafayt323@gmail.com>
2024-06-19 18:28:14 +0300

    feat(nvim): add transparent.nvim plug

--- config/nvim/lua/plugins/editor.lua
+++ config/nvim/lua/plugins/editor.lua
@@ -1,14 +1,18 @@
 return {
-  {
-    "folke/flash.nvim",
-    enabled = false,
-  },
-  {
-    "nvim-telescope/telescope.nvim",
-    keys = {
-      { "<leader>fg", "<cmd>Telescope live_grep<CR>", desc = "Live Grep" },
-    },
-  },
-  { "wakatime/vim-wakatime", lazy = false },
-  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+	{
+		"folke/flash.nvim",
+		enabled = false,
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		keys = {
+			{ "<leader>fg", "<cmd>Telescope live_grep<CR>", desc = "Live Grep" },
+		},
+	},
+	{ "wakatime/vim-wakatime", lazy = false },
+	{ "nvim-neo-tree/neo-tree.nvim", enabled = false },
+	{
+		"xiyaowong/transparent.nvim",
+		lazy = false,
+	},
 }
