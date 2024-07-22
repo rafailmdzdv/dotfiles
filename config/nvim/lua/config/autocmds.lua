97890cc0a9c0bbbeb7ec87861c9f12a94e90483c
rafailmdzdv <rafayt323@gmail.com>
2024-07-22 21:59:22 +0300

    fix(nvim): disable autoformat on c++ and c files

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "python", "toml" },
+  pattern = { "python", "toml", "cpp", "c" },
   callback = function()
     vim.b.autoformat = false
   end,
