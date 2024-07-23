918f26ccc6f9344509ad67b61b51a1f4dd423a83
rafailmdzdv <rafayt323@gmail.com>
2024-07-23 21:12:17 +0300

    fix(nvim): add clangd lsp

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -24,6 +24,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.java" },
     { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.clangd"},
 
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     -- import/override with your plugins
