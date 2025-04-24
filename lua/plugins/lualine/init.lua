0e2e14c8a6acf32d4b43af326a26f39d8294c631
rafailmdzdv <rafayt323@gmail.com>
2025-04-24 21:59:00 +0300

    disable kanagawa theme for lualine

--- lua/plugins/lualine/init.lua
+++ lua/plugins/lualine/init.lua
@@ -10,7 +10,7 @@ return {
         lualine.setup({
             options = {
                 icons_enabled = true,
-                theme = require("plugins.lualine.themes.kanagawa").theme,
+                -- theme = require("plugins.lualine.themes.kanagawa").theme,
                 component_separators = { left = "", right = "" },
                 section_separators = { left = "", right = "" },
                 disabled_filetypes = {
