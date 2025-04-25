6816881c054f74f38c47cad024d4b0d51574dfee
rafayt323@gmail.com <rafayt323@gmail.com>
2025-04-25 08:28:59 +0300

    add org roam

--- lua/plugins/orgmode.lua
+++ lua/plugins/orgmode.lua
@@ -1,12 +1,29 @@
 return {
-    "nvim-orgmode/orgmode",
-    event = "VeryLazy",
-    ft = { "org" },
-    config = function()
-        -- Setup orgmode
-        require("orgmode").setup({
-            org_agenda_files = "~/orgfiles/**/*",
-            org_default_notes_file = "~/orgfiles/refile.org",
-        })
-    end,
+    {
+        "nvim-orgmode/orgmode",
+        event = "VeryLazy",
+        ft = { "org" },
+        config = function()
+            -- Setup orgmode
+            require("orgmode").setup({
+                org_agenda_files = "~/orgfiles/**/*",
+                org_default_notes_file = "~/orgfiles/refile.org",
+            })
+        end,
+    },
+    {
+        "chipsenkbeil/org-roam.nvim",
+        tag = "0.1.1",
+        dependencies = {
+            {
+                "nvim-orgmode/orgmode",
+                tag = "0.3.7",
+            },
+        },
+        config = function()
+            require("org-roam").setup({
+                directory = "~/orgfiles",
+            })
+        end,
+    },
 }
