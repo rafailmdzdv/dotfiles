aa5d01d0b698cde8c5e18d4f497146b46e1f7ce6
rafailmdzdv <rafayt323@gmail.com>
2025-08-18 14:20:06 +0300

    add emacs cfg

---
+++ .emacs.d/init.el
@@ -0,0 +1,92 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+
+(package-initialize)
+
+(use-package lsp-mode
+  :ensure t
+  :hook (python-mode))
+
+(use-package lsp-pyright
+  :ensure t
+  :custom (lsp-pyright-langserver-command "pyright") ;; or basedpyright
+  :hook (python-mode . (lambda ()
+                          (require 'lsp-pyright)
+                          (lsp))))  ; or lsp-deferred
+
+(use-package lsp-ui
+  :ensure t)
+
+(use-package flycheck
+  :ensure t)
+
+(use-package tree-sitter
+  :ensure t
+  :hook ((python-mode) . tree-sitter-hl-mode)
+  :config
+  (global-tree-sitter-mode))
+
+(use-package tree-sitter-langs
+  :ensure t)
+
+(use-package hl-todo
+  :ensure t)
+
+(global-hl-todo-mode t)
+
+(use-package vertico
+  :ensure t
+  :init
+  (vertico-mode))
+
+(use-package company
+  :ensure t
+  :hook (python-mode))
+
+(use-package dashboard
+  :ensure t
+  :config
+  (dashboard-setup-startup-hook))
+(setq dashboard-center-content t
+      dashboard-display-icons-p t
+      dashboard-icon-type 'nerd-icons
+      dashboard-set-heading-icons t
+      dashboard-set-file-icons t)
+
+(use-package nerd-icons
+  :ensure t)
+
+(use-package magit
+  :ensure t)
+
+(use-package evil
+  :ensure t)
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(load-theme 'rose-pine t)
+
+(evil-mode t)
+
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("c4a66d0d9557de08eadd6cc25112bd7eb776d8c8754ed8cc4e1b16d0c317433c"
+	 default))
+ '(display-line-numbers 'relative)
+ '(make-backup-files nil)
+ '(menu-bar-mode nil)
+ '(package-selected-packages nil)
+ '(tab-bar-show t)
+ '(tab-width 4)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#1F1D2E" :foreground "#e0def4" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :family "Maple Mono NF")))))
