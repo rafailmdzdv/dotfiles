9e58c3a8c89120794d42b6851957afece7820b80
rafailmdzdv <rafayt323@gmail.com>
2025-10-23 21:39:55 +0300

    change font size

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -262,7 +262,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "#1F1D2E" :foreground "#e0def4" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :family "Maple Mono NF"))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#1F1D2E" :foreground "#e0def4" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :family "Maple Mono NF"))))
  '(lsp-headerline-breadcrumb-path-face ((t (:background "#191724"))))
  '(lsp-headerline-breadcrumb-separator-face ((t (:background "#191724"))))
  '(lsp-headerline-breadcrumb-symbols-face ((t (:background "#191724")))))
